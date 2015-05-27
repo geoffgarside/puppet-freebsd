fe1c89e815d1d59c67066b7a3c930a9df858466a
Geoff Garside <geoff@geoffgarside.co.uk>
2015-05-27 21:56:01 +0100

    Cleanup cpu_supports_aesni fact

--- lib/facter/cpu_supports_aesni.rb
+++ lib/facter/cpu_supports_aesni.rb
@@ -2,6 +2,6 @@ require 'facter'
 Facter.add(:cpu_supports_aesni) do
   confine :kernel => 'FreeBSD'
   setcode do
-    Facter.value(:cpu_features).include?("AESNI")
+    Facter.value(:cpu_features).include?('AESNI')
   end
 end
