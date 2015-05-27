85e860fbf74e2585ee8411a8d4e392eba8141e76
Geoff Garside <geoff@geoffgarside.co.uk>
2015-05-27 21:55:46 +0100

    Make cpu_features fact tolerant of missing files
    Returns an empty array of features if the /var/run/dmesg.boot file is not present.

--- lib/facter/cpu_features.rb
+++ lib/facter/cpu_features.rb
@@ -3,10 +3,16 @@ Facter.add(:cpu_features) do
   confine :kernel => 'FreeBSD'
   setcode do
     features = []
+    needle   = 'Features'
+    path     = '/var/run/dmesg.boot'
+    cmd      = '/usr/bin/grep'
 
-    Facter::Core::Execution.exec('/usr/bin/grep Features /var/run/dmesg.boot').each_line do |line|
+    return features unless File.exist?(path)
+
+    grep = Facter::Core::Execution.exec("#{cmd} #{needle} #{path}")
+    grep.each_line do |line|
       next unless line =~ /\sFeatures[0-9]*=0x(?:[0-9a-f]+)<([^>]+)>/
-      features += $1.split(',')
+      features += Regexp.last_match(1).split(',')
     end
 
     features.sort
