4d0e00c14d81026507dda5dddadaef2bf92d4db3
Geoff Garside <geoff@geoffgarside.co.uk>
2015-05-27 22:09:33 +0100

    Correct cpu_features fact
    Can't 'return' from setcode block.

--- lib/facter/cpu_features.rb
+++ lib/facter/cpu_features.rb
@@ -7,12 +7,12 @@ Facter.add(:cpu_features) do
     path     = '/var/run/dmesg.boot'
     cmd      = '/usr/bin/grep'
 
-    return features unless File.exist?(path)
-
-    grep = Facter::Core::Execution.exec("#{cmd} #{needle} #{path}")
-    grep.each_line do |line|
-      next unless line =~ /\sFeatures[0-9]*=0x(?:[0-9a-f]+)<([^>]+)>/
-      features += Regexp.last_match(1).split(',')
+    if File.exist?(path) && File.exist?(cmd)
+      grep = Facter::Core::Execution.exec("#{cmd} #{needle} #{path}")
+      grep.each_line do |line|
+        next unless line =~ /\sFeatures[0-9]*=0x(?:[0-9a-f]+)<([^>]+)>/
+        features += Regexp.last_match(1).split(',')
+      end
     end
 
     features.sort
